5bc3f54c3ed98b516939f27493b772d4ec08721c
Joey Gouly <cardologist@gmail.com>
2012-03-30 18:10:51 +0100

    Switch to constexpr

--- chp1/ex1.12.cpp
+++ chp1/ex1.12.cpp
@@ -1,21 +1,23 @@
+// -std=c++11
 template<int Row, int Column>
 struct Pascal {
-  enum { Value = Pascal<Row-1, Column-1>::Value + Pascal<Row-1, Column>::Value };
+  constexpr static unsigned Value = 
+    Pascal<Row - 1, Column - 1>::Value + Pascal<Row - 1, Column>::Value; 
 };
 
 template<int Column>
 struct Pascal<1, Column> {
-  enum { Value = 1 };
+  constexpr static unsigned Value = 1;
 };
 
 template<int Row>
 struct Pascal<Row, 1> {
-  enum { Value = 1 };
+  constexpr static unsigned Value = 1;
 };
 
 template<int Same>
 struct Pascal<Same, Same> {
-  enum { Value = 1 };
+  constexpr static unsigned Value = 1;
 };
 
 int main()
