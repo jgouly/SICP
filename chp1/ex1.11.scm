0648a5cf2171db2ab44398364a8e947f64d563b1
Joey Gouly <cardologist@gmail.com>
2012-03-30 17:23:31 +0100

    Add iterative version

--- chp1/ex1.11.scm
+++ chp1/ex1.11.scm
@@ -4,3 +4,12 @@
       (+ (f (- n 1))
 	 (* 2 (f (- n 2)))
 	 (* 3 (f (- n 3))))))
+
+(define (f-iter a b c count)
+  (if (= 2 count)
+      a
+      (f-iter (+ a (* 2 b) (* 3 c))
+	      a b (- count 1))))
+
+(define (f-iter-outer n)
+  (f-iter 2 1 0 n))
