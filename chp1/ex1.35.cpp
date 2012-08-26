bbdec0537fbeb0f51fc3ed9b30d070b7f3d243a3
joey <cardologist@gmail.com>
2012-08-26 23:29:51 +0100

    Add ex1.36 to ex1.35.cpp

--- chp1/ex1.35.cpp
+++ chp1/ex1.35.cpp
@@ -13,11 +13,14 @@ bool close_enough(double a, double b)
 double fixed_point(std::function<double(double)> f, double guess)
 {
 	double next = f(guess);
+	int steps = 1;
 	while(!close_enough(guess, next))
 	{
 		guess = next;
 		next = f(guess);
+		++steps;
 	}
+	std::cout << steps << " steps" << std::endl;
 	return guess;	
 }
 
@@ -25,5 +28,7 @@ int main()
 {
 	std::cout << std::setprecision(10);
 	std::cout << fixed_point([](double n){ return 1+1.0/n;}, 1.0) << std::endl;
+	std::cout << fixed_point([](double n) { return log(1000)/log(n);}, 2.0) << std::endl;
+	std::cout << fixed_point([](double n) { return (n+log(1000)/log(n))/2.0;}, 2.0) << std::endl;
 	return 0;
 }
