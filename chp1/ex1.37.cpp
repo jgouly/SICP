21cec2ddeac2814e575c7fa7c5fe69750c670128
joey <cardologist@gmail.com>
2012-08-28 00:30:09 +0100

    Added ex1.38 to ex1.37

--- chp1/ex1.37.cpp
+++ chp1/ex1.37.cpp
@@ -3,11 +3,20 @@
 
 double continued_fraction(std::function<double(double)> n,
 	std::function<double(double)> d, int k, int level = 1) {
+	std::cout << level << " " << d(level) << std::endl;
 	if(k == level) return n(k) / d(k);
-	return n(k) / (d(k) + continued_fraction(n, d, k, level + 1));
+	return n(level) / (d(level) + continued_fraction(n, d, k, level + 1));
+}
+
+double euler(int n) {
+	if(n < 3) return n;
+	if(n == 3) return 1;
+	if((n+1) % 3 == 0) return 2*(n+1)/3;
+	return 1;
 }
 
 int main() {
 	std::cout << 1 / continued_fraction([](double){return 1.0;}, [](double){return 1.0;}, 10) << std::endl;
+	std::cout << continued_fraction([](double){return 1.0;}, euler, 15)+2 << std::endl;
 	return 0;
 }
