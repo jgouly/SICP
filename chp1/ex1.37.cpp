222ad1866981bbe69fc279d4227b64bd4b965897
joey <cardologist@gmail.com>
2012-08-28 00:36:31 +0100

    Remove print line

--- chp1/ex1.37.cpp
+++ chp1/ex1.37.cpp
@@ -1,5 +1,6 @@
 #include <functional>
 #include <iostream>
+#include <cmath>
 
 double continued_fraction(std::function<double(double)> n,
 	std::function<double(double)> d, int k, int level = 1) {
@@ -14,8 +15,19 @@ double euler(int n) {
 	return 1;
 }
 
+double square(int n) { return n * n; }
+
+double odd(int n) {	return (3*n) - 1; }
+
+double tan_cf(double t, int k)
+{
+	return continued_fraction([=](int n){return t*(1==n)?1:n;}, odd, k);
+}
+
 int main() {
 	std::cout << 1 / continued_fraction([](double){return 1.0;}, [](double){return 1.0;}, 10) << std::endl;
 	std::cout << continued_fraction([](double){return 1.0;}, euler, 15)+2 << std::endl;
+	std::cout << tan(10.5) << std::endl;
+	std::cout << tan_cf(10.5, 100) << std::endl; 
 	return 0;
 }
