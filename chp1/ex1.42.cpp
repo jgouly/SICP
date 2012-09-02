db3cfd64eccdd3bd6117d0868f70fe23e5fcd8fe
joey <cardologist@gmail.com>
2012-09-02 16:34:31 +0100

    Add Ex 1.43 to ex1.42.cpp

--- chp1/ex1.42.cpp
+++ chp1/ex1.42.cpp
@@ -11,10 +11,33 @@ struct compose {
 int inc(int x) { return x + 1; }
 int square(int x) { return x * x; }
 
-int main() {
-	compose<int> squinc = { square, inc };
+template<int N, typename T>
+struct inner_repeat {
+	std::function<T(T)> get(std::function<T(T)> f)
+	{
+		return inner_repeat<N - 1, T>().get(compose<T> { f, f });
+	}
+};
 
+template<typename T>
+struct inner_repeat<1, T> {
+	std::function<T(T)> get(std::function<T(T)> f)
+	{
+		return f;
+	}
+};
+
+template<typename T, int N>
+std::function<T(T)> repeat(std::function<T(T)> f)
+{
+	return inner_repeat<N, T>().get(f);
+}
+
+int main() {
+	compose<int> squinc { square, inc };
 	std::cout << squinc(6) << std::endl;
 
+	auto repeated = repeat<int, 2>(square);
+	std::cout << repeated(5) << std::endl;
 	return 0;
 }
